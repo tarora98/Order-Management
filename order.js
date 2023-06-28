9d318177e9423e6b7417c4f75796f53ea09857ed
Tarun Arora <46683543+tarora98@users.noreply.github.com>
2023-06-28 18:50:07 +0530

    Update order.js

--- order.js
+++ order.js
@@ -1,106 +1,134 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+import express, { Request, Response } from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 app.use(express.json());
 
-const orders = [
+// PostgreSQL database connection
+const pool = new Pool({
+    user: 'your_username',
+    host: 'your_host',
+    database: 'your_database',
+    password: 'your_password',
+    port: 5432,
+});
+
+// Sample data
+const serviceRecords = [
     {
-        id: '223',
-        datetime: '2022-11-01T11:11:11.111Z',
-        totalfee: 100,
-        services: [{ id: '123' }],
+        id: 123,
+        name: 'Inspection',
     },
     {
-        id: '224',
-        datetime: '2022-11-01T11:11:11.111Z',
-        totalfee: 100,
-        services: [{ id: '789' }],
+        id: 789,
+        name: 'Testing',
     },
     {
-        id: '225',
-        datetime: '2022-11-01T11:11:11.111Z',
-        totalfee: 100,
-        services: [{ id: '456' }],
+        id: 456,
+        name: 'Analysis',
     },
 ];
 
-// Get all orders
-app.get('/orders', (req, res) => {
-    res.json(orders);
-});
+// Endpoint to create a new order
+app.post('/orders', async (req: Request, res: Response) => {
+    try {
+        const { id, datetime, totalfee, services } = req.body;
 
-// Get a specific order by ID
-app.get('/orders/:id', (req, res) => {
-    const { id } = req.params;
-    const order = orders.find((order) => order.id === id);
-
-    if (order) {
-        res.json(order);
-    } else {
-        res.status(404).json({ error: 'Order not found' });
-    }
-});
-
-// Create a new order
-app.post('/orders', (req, res) => {
-    const { id, datetime, totalfee, services } = req.body;
-
-    // Check if there is an existing order within 3 hours
-    const existingOrder = orders.find(
-        (order) => Math.abs(new Date(order.datetime) - new Date(datetime)) < 3 * 60 * 60 * 1000
-    );
-
-    if (existingOrder) {
-        res.status(400).json({ error: 'An order already exists within 3 hours of the specified datetime' });
-    } else {
-        const newOrder = { id: uuidv4(), datetime, totalfee, services };
-        orders.push(newOrder);
-        res.status(201).json(newOrder);
-    }
-});
-
-// Update an existing order
-app.put('/orders/:id', (req, res) => {
-    const { id } = req.params;
-    const { datetime, totalfee, services } = req.body;
-
-    const orderIndex = orders.findIndex((order) => order.id === id);
-
-    if (orderIndex !== -1) {
-        // Check if there is an existing order within 3 hours
-        const existingOrder = orders.find(
-            (order) =>
-                order.id !== id &&
-                Math.abs(new Date(order.datetime) - new Date(datetime)) < 3 * 60 * 60 * 1000
+        // Check if there is a pre-existing order within the last 3 hours
+        const existingOrder = await pool.query(
+            'SELECT * FROM orders WHERE datetime >= NOW() - INTERVAL \'3 hours\''
         );
 
-        if (existingOrder) {
-            res.status(400).json({ error: 'An order already exists within 3 hours of the specified datetime' });
-        } else {
-            orders[orderIndex] = { id, datetime, totalfee, services };
-            res.json({ message: 'Order updated successfully' });
+        if (existingOrder.rowCount > 0) {
+            return res.status(400).json({ error: 'An order already exists within the last 3 hours' });
         }
-    } else {
-        res.status(404).json({ error: 'Order not found' });
+
+        await pool.query('INSERT INTO orders (id, datetime, totalfee, services) VALUES ($1, $2, $3, $4)', [
+            id,
+            datetime,
+            totalfee,
+            JSON.stringify(services),
+        ]);
+
+        res.status(201).json({ message: 'Order created successfully' });
+    } catch (error) {
+        console.error('Error creating order:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
-// Delete an order
-app.delete('/orders/:id', (req, res) => {
-    const { id } = req.params;
-    const orderIndex = orders.findIndex((order) => order.id === id);
+// Endpoint to get details of a specific order
+app.get('/orders/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
 
-    if (orderIndex !== -1) {
-        orders.splice(orderIndex, 1);
+        const result = await pool.query('SELECT * FROM orders WHERE id = $1', [id]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error retrieving order:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Endpoint to update details of a specific order
+app.put('/orders/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const { datetime, totalfee, services } = req.body;
+
+        const result = await pool.query(
+            'UPDATE orders SET datetime = $1, totalfee = $2, services = $3 WHERE id = $4',
+            [datetime, totalfee, JSON.stringify(services), id]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
+        res.json({ message: 'Order updated successfully' });
+    } catch (error) {
+        console.error('Error updating order:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Endpoint to delete a specific order
+app.delete('/orders/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+
+        const result = await pool.query('DELETE FROM orders WHERE id = $1', [id]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
         res.json({ message: 'Order deleted successfully' });
-    } else {
-        res.status(404).json({ error: 'Order not found' });
+    } catch (error) {
+        console.error('Error deleting order:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Endpoint to get all orders
+app.get('/orders', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query('SELECT * FROM orders');
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error retrieving orders:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
